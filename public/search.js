1244a06f4d4e40d821e67d239ecb351b04ee3b4e
tantran <octttran@gmail.com>
2015-12-29 03:52:30 -0800

    clean up css and DOM elements for all requests

--- public/search.js
+++ public/search.js
@@ -113,34 +113,57 @@ clickSearch.addEventListener('click', function(e) {
       searchResult.innerHTML = "";
 
       for (i=0; i<apiResponse.results.length; i++) {
-        
         var divOne = document.createElement('div');
         divOne.setAttribute('class', 'col-md-6');
         searchResult.appendChild(divOne);
 
+        var divEight = document.createElement('div');
+        divEight.setAttribute('class', 'col-md-12 mainPopular');
+        divOne.appendChild(divEight);
+
         var divTwo = document.createElement('div');
-        divTwo.setAttribute('class', 'col-md-6 img-responsive thumbnail');
-        divOne.appendChild(divTwo);
+        divTwo.setAttribute('class', 'col-md-6');
+        divEight.appendChild(divTwo);
 
         var divThree = document.createElement('div');
         divThree.setAttribute('class', 'col-md-6');
-        divOne.appendChild(divThree);
+        divEight.appendChild(divThree);
 
         var imageOne = document.createElement('img');
+        imageOne.setAttribute('class', 'image');
         imageOne.setAttribute('src', 'http://image.tmdb.org/t/p/w500' + apiResponse.results[i].poster_path)
         divTwo.appendChild(imageOne);
 
-        var divFive = document.createElement('div');
-        divFive.setAttribute('class', 'col-md-10');
-        var headerOne = document.createElement('h4');
-        headerOne.textContent = apiResponse.results[i].title;
-        divThree.appendChild(divFive);
-        divFive.appendChild(headerOne);
-        
+        var divSeven = document.createElement('div');
+        divSeven.setAttribute('id', 'seven');
+        divThree.appendChild(divSeven);
 
+        var divFive = document.createElement('div');
+        divFive.setAttribute('class', 'col-md-8');
+        var headerOne = document.createElement('h4');
+        headerOne.setAttribute('class', 'title');
+        headerOne.textContent = apiResponse.results[i].title;
+        divSeven.appendChild(divFive);
+        divFive.appendChild(headerOne);
+
+        var divFour = document.createElement('div');
+        divFour.setAttribute('class', 'rating col-md-4');
+        var paraTwo = document.createElement('p');
+        paraTwo.textContent = apiResponse.results[i].vote_average;
+        divSeven.appendChild(divFour);
+        divFour.appendChild(paraTwo);
+
+        var rating = document.createElement('span');
+        rating.setAttribute('class', 'fa fa-star');
+        paraTwo.appendChild(rating);
+
+        var divSix = document.createElement('div')
+        divSix.setAttribute('class', 'col-md-12');
         var paraOne = document.createElement('p');
-        divThree.appendChild(paraOne);
+        paraOne.setAttribute('class', 'overview');
         paraOne.textContent = apiResponse.results[i].overview;
+        divSix.appendChild(paraOne);
+        divThree.appendChild(divSix);
 
   //     var searchResult = "";
   //     apiResponse.results.forEach(function(value, index) {
@@ -186,30 +209,57 @@ clickPopular.addEventListener('click', function(e) {
       searchResult.innerHTML = "";
       
       for (i=0; i<apiResponse.results.length; i++) {
-        
         var divOne = document.createElement('div');
         divOne.setAttribute('class', 'col-md-6');
         searchResult.appendChild(divOne);
 
+        var divEight = document.createElement('div');
+        divEight.setAttribute('class', 'col-md-12 mainPopular');
+        divOne.appendChild(divEight);
+
         var divTwo = document.createElement('div');
-        divTwo.setAttribute('class', 'col-md-6 img-responsive thumbnail');
-        divOne.appendChild(divTwo);
+        divTwo.setAttribute('class', 'col-md-6');
+        divEight.appendChild(divTwo);
 
         var divThree = document.createElement('div');
         divThree.setAttribute('class', 'col-md-6');
-        divOne.appendChild(divThree);
+        divEight.appendChild(divThree);
 
         var imageOne = document.createElement('img');
+        imageOne.setAttribute('class', 'image');
         imageOne.setAttribute('src', 'http://image.tmdb.org/t/p/w500' + apiResponse.results[i].poster_path)
         divTwo.appendChild(imageOne);
 
-        var headerOne = document.createElement('h4');
-        divThree.appendChild(headerOne);
-        headerOne.textContent = apiResponse.results[i].title;
+        var divSeven = document.createElement('div');
+        divSeven.setAttribute('id', 'seven');
+        divThree.appendChild(divSeven);
 
+        var divFive = document.createElement('div');
+        divFive.setAttribute('class', 'col-md-8');
+        var headerOne = document.createElement('h4');
+        headerOne.setAttribute('class', 'title');
+        headerOne.textContent = apiResponse.results[i].title;
+        divSeven.appendChild(divFive);
+        divFive.appendChild(headerOne);
+
+        var divFour = document.createElement('div');
+        divFour.setAttribute('class', 'rating col-md-4');
+        var paraTwo = document.createElement('p');
+        paraTwo.textContent = apiResponse.results[i].vote_average;
+        divSeven.appendChild(divFour);
+        divFour.appendChild(paraTwo);
+
+        var rating = document.createElement('span');
+        rating.setAttribute('class', 'fa fa-star');
+        paraTwo.appendChild(rating);
+
+        var divSix = document.createElement('div')
+        divSix.setAttribute('class', 'col-md-12');
         var paraOne = document.createElement('p');
-        divThree.appendChild(paraOne);
+        paraOne.setAttribute('class', 'overview');
         paraOne.textContent = apiResponse.results[i].overview;
+        divSix.appendChild(paraOne);
+        divThree.appendChild(divSix);
 
   //     var searchResult = "";
   //     apiResponse.results.forEach(function(value, index) {
@@ -248,33 +298,59 @@ clickUpComing.addEventListener('click', function(e) {
       var apiResponse = JSON.parse(xhr.responseText);
 
       searchResult.innerHTML = "";
-  
-      for (i=0; i<apiResponse.results.length; i++) {
         
+        for (i=0; i<apiResponse.results.length; i++) {
         var divOne = document.createElement('div');
         divOne.setAttribute('class', 'col-md-6');
         searchResult.appendChild(divOne);
 
+        var divEight = document.createElement('div');
+        divEight.setAttribute('class', 'col-md-12 mainPopular');
+        divOne.appendChild(divEight);
+
         var divTwo = document.createElement('div');
-        divTwo.setAttribute('class', 'col-md-6 img-responsive thumbnail');
-        divOne.appendChild(divTwo);
+        divTwo.setAttribute('class', 'col-md-6');
+        divEight.appendChild(divTwo);
 
         var divThree = document.createElement('div');
         divThree.setAttribute('class', 'col-md-6');
-        divOne.appendChild(divThree);
+        divEight.appendChild(divThree);
 
         var imageOne = document.createElement('img');
+        imageOne.setAttribute('class', 'image');
         imageOne.setAttribute('src', 'http://image.tmdb.org/t/p/w500' + apiResponse.results[i].poster_path)
         divTwo.appendChild(imageOne);
 
+        var divSeven = document.createElement('div');
+        divSeven.setAttribute('id', 'seven');
+        divThree.appendChild(divSeven);
+
+        var divFive = document.createElement('div');
+        divFive.setAttribute('class', 'col-md-8');
         var headerOne = document.createElement('h4');
-        divThree.appendChild(headerOne);
+        headerOne.setAttribute('class', 'title');
         headerOne.textContent = apiResponse.results[i].title;
+        divSeven.appendChild(divFive);
+        divFive.appendChild(headerOne);
 
+        var divFour = document.createElement('div');
+        divFour.setAttribute('class', 'rating col-md-4');
+        var paraTwo = document.createElement('p');
+        paraTwo.textContent = apiResponse.results[i].vote_average;
+        divSeven.appendChild(divFour);
+        divFour.appendChild(paraTwo);
+
+        var rating = document.createElement('span');
+        rating.setAttribute('class', 'fa fa-star');
+        paraTwo.appendChild(rating);
+
+        var divSix = document.createElement('div')
+        divSix.setAttribute('class', 'col-md-12');
         var paraOne = document.createElement('p');
-        divThree.appendChild(paraOne);
+        paraOne.setAttribute('class', 'overview');
         paraOne.textContent = apiResponse.results[i].overview;
-
+        divSix.appendChild(paraOne);
+        divThree.appendChild(divSix);
   //     var searchResult = "";
   //     apiResponse.results.forEach(function(value, index) {
   //     searchResult += "<div class='col-md-6'>" +
